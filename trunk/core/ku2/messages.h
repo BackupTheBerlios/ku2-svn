e6e699c333697a0b24fe62c4f6ade9b757426a7c
kane <kane@06800212-dd1b-0410-9835-f82aa0ab8865>
2008-04-21 15:57:27 +0000

    version 1.7.0; added macros for messaging (similar to try-catch);

---
+++ trunk/core/ku2/messages.h
@@ -0,0 +1,57 @@
+/*
+		messages.h
+		Mon Apr 21 18:16:59 2008
+
+	This file is the part of Kane Utilities 2.
+	See licencing agreement in a root direcory for more details.
+	http://developer.berlios.de/projects/ku2/
+
+	Copyright, 2008
+		J. Anton (Jeļkins Antons) aka Kane
+		kane@mail.berlios.de
+*/
+
+/*!
+	\file
+	\brief BRIEF.
+	
+	DESC.
+	\author J. Anton
+*/
+
+#ifndef KU__MESSAGES_H__
+#define KU__MESSAGES_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define KU_MESSAGE __ku_message
+
+#define KU_MESSAGE_TYPE( __type ) \
+__type KU_MESSAGE
+
+#define KU_WATCH \
+{ \
+	__label__ __ku_parse_end; \
+	{ \
+		__label__ __ku_parse_begin;
+
+#define KU_PARSE \
+		goto __ku_parse_end; \
+		__ku_parse_begin:; \
+	}
+
+#define KU_ENDWATCH \
+	__ku_parse_end: asm(""); \
+}
+
+#define KU_MAKEMSG( __message ) \
+{ \
+	KU_MESSAGE = __message; \
+	goto __ku_parse_begin; \
+}
+
+#ifdef __cplusplus
+}
+#endif
+#endif
